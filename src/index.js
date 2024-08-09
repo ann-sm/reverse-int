4b8a75b5f4acf9eb21509856348cfcc7272308d5
ann-sm <annsmirnovva@gmail.com>
2024-08-09 19:40:51 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+let i = 0;
+let result = '';
+let resultNumber = '';  
+
+let m = Math.abs((n));
+
+while (i < m.toString().length) {
+    resultNumber = m.toString()[i];
+    result = `${resultNumber}${result}`;
+    i = i + 1;
+}
+
+return result;
+
 }
